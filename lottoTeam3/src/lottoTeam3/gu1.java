7454a1e3d15137638e5b6773813fca89210f735a
LeeKangHo1 <kangho.lee.pr@gmail.com>
2024-07-26 12:15:49 +0900

    충돌확인

--- lottoTeam3/src/lottoTeam3/gu1.java
+++ lottoTeam3/src/lottoTeam3/gu1.java
@@ -1,5 +1,15 @@
 package lottoTeam3;
 
-public class gu1 {
+class Test1234 {
+	private int a;
+	private int b;
+	private int c;
+	private int d;
+	private int e;
+}
 
+public class gu1 {
+	public static void main(String[] args) {
+		System.out.println("충돌 해결");
+	}
 }
