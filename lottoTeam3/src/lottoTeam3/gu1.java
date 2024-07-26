dedd7ea91d94ed7ffc8f26447145a424a9f76579
LeeKangHo1 <kangho.lee.pr@gmail.com>
2024-07-26 12:18:54 +0900

    master브랜치에 병합

--- lottoTeam3/src/lottoTeam3/gu1.java
+++ lottoTeam3/src/lottoTeam3/gu1.java
@@ -1,15 +1,9 @@
 package lottoTeam3;
 
-class Test1234 {
-	private int a;
-	private int b;
-	private int c;
-	private int d;
-	private int e;
-}
 
 public class gu1 {
 	public static void main(String[] args) {
 		System.out.println("충돌 해결");
+		System.out.println("1차 확인");
 	}
 }
