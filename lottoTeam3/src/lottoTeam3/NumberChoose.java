946e9ffbf2777a546f8b5bbce9282de9162a8aaa
goho11 <goav550@gmail.com>
2024-07-26 17:59:56 +0900

    로또 숫자 선택 버튼을 크기와 폰트 조절했습니다

--- lottoTeam3/src/lottoTeam3/NumberChoose.java
+++ lottoTeam3/src/lottoTeam3/NumberChoose.java
@@ -1,7 +1,10 @@
 package lottoTeam3;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -9,25 +12,37 @@ import javax.swing.JPanel;
 
 public class NumberChoose extends JDialog {
 	public NumberChoose() {
+		setTitle("로또 번호 선택");
 
-		setLayout(new BorderLayout());
-		setSize(500, 500);
-
-		//String[] btnText = { "1", "2", "3" };
+//		setLayout(new BorderLayout());
+		setSize(421, 280);
 
 		JPanel pnl = new JPanel(new BorderLayout());
+		// 패널A : 1~45 숫자범위
+		JPanel pnlA = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		// 패너류 : 기능 안내 버튼
+		JPanel pnlB = new JPanel();
 		JButton[] btn = new JButton[45];
 		JButton check = new JButton("확인");
 		JButton reset = new JButton("초기화");
 		JButton auto = new JButton("자동");
 
-		// BorderLayout
-		add(pnl, "Center");
-		pnl.setLayout(new GridLayout(6, 9));
 		// 배열은 같다고 하면 안됨. 마지막 번호를 추가하고 싶으면 -1
+		Font font = new Font("맑은 고딕", Font.BOLD, 18);
 		for (int i = 0; i < btn.length; i++) {
-			pnl.add(btn[i] = new JButton(String.valueOf(i+1)));
+			pnlA.add(btn[i] = new JButton(String.valueOf(i + 1)));
+			btn[i].setPreferredSize(new Dimension(35, 35));
+			btn[i].setFont(font);
+			btn[i].setMargin(new Insets(0, 0, 0, 0));
 		}
+
+		pnlB.add(check);
+		pnlB.add(reset);
+		pnlB.add(auto);
+
+		pnl.add(pnlA);
+		pnl.add(pnlB, "South");
+		add(pnl);
 	}
 
 	public static void main(String[] args) {
