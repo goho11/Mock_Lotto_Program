0740e756b646186489977ebc983bb2864566ad26
HSLee1013 <fantasytotal@naver.com>
2024-07-30 17:40:56 +0900

    모드 한글 추가

--- lottoTeam3/src/lottoTeam3/Mode.java
+++ lottoTeam3/src/lottoTeam3/Mode.java
@@ -3,5 +3,15 @@ package lottoTeam3;
 // NumberChoose에 활용
 // 로또 번호 선택 기능  : 자동, 반자동, 수동
 public enum Mode {
-	AUTO, SEMI, MANUAL;
+	AUTO("자동"), SEMI("반자동"), MANUAL("수동");
+
+	private String korean;
+
+	private Mode(String korean) {
+		this.korean = korean;
+	}
+
+	public String getKorean() {
+		return korean;
+	}
 }
