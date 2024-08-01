d8fe9529303d08ba5f72bcb41e64785f9a898ca6
goho11 <goav550@gmail.com>
2024-08-01 18:00:07 +0900

    글씨 폰트적용과 크기 통일

--- lottoTeam3/src/lottoTeam3/GGG.java
+++ lottoTeam3/src/lottoTeam3/GGG.java
@@ -3,6 +3,8 @@ package lottoTeam3;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,7 +19,6 @@ public class GGG extends JFrame implements ActionListener {
 	public GGG() {
 		super("가상 로또 시뮬레이션");
 
-		
 		JPanel pnl = new JPanel(new BorderLayout());
 		JPanel pnlPic = new JPanel();
 		JPanel pnlBtn = new JPanel(null);
@@ -27,29 +28,44 @@ public class GGG extends JFrame implements ActionListener {
 		JLabel lblLotto = new JLabel(new ImageIcon(GGG.class.getResource("/resource/lotto.png")));
 		pnlPic.add(lblLotto);
 
-		JButton buy = new JButton("구매(n장)");
+		JButton buy = new JButton("구매(0장)");
 		buy.setBounds(20, 30, 200, 80);
 		buy.setBackground(Color.WHITE);
+		buy.setFont(FontHolder.getInstance().getDeriveFont(Font.PLAIN, 34));
+		buy.setMargin(new Insets(0, 0, 0, 0));
+		buy.setFocusable(false);
 		pnlBtn.add(buy);
 
 		JButton buyCheck = new JButton("현재 구매 확인");
 		buyCheck.setBounds(20, 110, 200, 80);
 		buyCheck.setBackground(Color.WHITE);
+		buyCheck.setFont(FontHolder.getInstance().getDeriveFont(Font.PLAIN, 30));
+		buyCheck.setMargin(new Insets(0, 0, 0, 0));
+		buyCheck.setFocusable(false);
 		pnlBtn.add(buyCheck);
 
-		JButton draw = new JButton("n회차 추첨");
+		JButton draw = new JButton("100회차 추첨");
 		draw.setBounds(220, 30, 200, 80);
 		draw.setBackground(Color.WHITE);
+		draw.setFont(FontHolder.getInstance().getDeriveFont(Font.PLAIN, 34));
+		draw.setMargin(new Insets(0, 0, 0, 0));
+		draw.setFocusable(false);
 		pnlBtn.add(draw);
 
 		JButton buyBefore = new JButton("이전 구매 확인");
 		buyBefore.setBounds(220, 110, 200, 80);
 		buyBefore.setBackground(Color.WHITE);
+		buyBefore.setFont(FontHolder.getInstance().getDeriveFont(Font.PLAIN, 30));
+		buyBefore.setMargin(new Insets(0, 0, 0, 0));
+		buyBefore.setFocusable(false);
 		pnlBtn.add(buyBefore);
 
 		JButton end = new JButton("종료");
 		end.setBounds(120, 210, 200, 50);
 		end.setBackground(Color.WHITE);
+		end.setFont(FontHolder.getInstance().getDeriveFont(Font.PLAIN, 34));
+		end.setMargin(new Insets(0, 0, 0, 0));
+		end.setFocusable(false);
 		pnlBtn.add(end);
 
 		pnl.add(pnlPic, "North");
