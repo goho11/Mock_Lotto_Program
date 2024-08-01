c886347c4c7de2998c6b345620eebbb07fbbc606
HSLee1013 <fantasytotal@naver.com>
2024-08-01 16:07:32 +0900

    이전회차 다이얼로그 클래스 틀만
    만듦

--- lottoTeam3/src/lottoTeam3/PrevLottoDialog.java
+++ lottoTeam3/src/lottoTeam3/PrevLottoDialog.java
@@ -6,12 +6,13 @@ import java.util.List;
 import javax.swing.JDialog;
 
 public class PrevLottoDialog extends JDialog {
-	
+
 	public PrevLottoDialog(Window window, List<LottoRecord> list) {
 
 	}
 
 	public static void showDialog(Window window, List<LottoRecord> list) {
 		PrevLottoDialog prevLottoDialog = new PrevLottoDialog(window, list);
+		prevLottoDialog.setVisible(true);
 	}
 }
