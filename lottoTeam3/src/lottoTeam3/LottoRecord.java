e7be9852792471f09ba13a4497331bb91adda6ad
goho11 <goav550@gmail.com>
2024-08-01 12:00:36 +0900

    수정전 올리기

--- lottoTeam3/src/lottoTeam3/LottoRecord.java
+++ lottoTeam3/src/lottoTeam3/LottoRecord.java
@@ -1,17 +1,18 @@
 package lottoTeam3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import javafx.beans.binding.StringBinding;
+
 public class LottoRecord {
-	private List<Integer> lotteryNums;
-	private int lotteryBonus;
-	private List<LottoData[]> buyLotto;
-	private List<Integer> prizeMoney;
-	private int totalPrizeMoney;
+	private List<Integer> lotteryNums; // 당첨 번호
+	private int lotteryBonus; // 당첨 보너스 번호
+	private List<LottoData[]> buyLotto; // 구매자가 선택한 번호
 
 	public LottoRecord() {
-		lotteryNums = new ArrayList<>();
+		lotteryNums = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 		buyLotto = new ArrayList<>();
 	}
 
@@ -25,19 +26,18 @@ public class LottoRecord {
 	}
 
 	public static LottoRecord tryParse(String line) {
+		
+		
 		return null;
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(lotteryNums).append('/').append(lotteryBonus).append('/');
 		for (LottoData[] lottoDatas : buyLotto) {
-			for (LottoData lottoData : lottoDatas) {
-				stringBuilder.append(lottoData).append(' ');
-			}
-			stringBuilder.append('\n');
+			stringBuilder.append(Arrays.toString(lottoDatas)).append('+');
 		}
-
 		return stringBuilder.toString();
 	}
 
